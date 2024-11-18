d4bc36851f5607c73f6cf4659b3b883c41852431
haradeuel <haradeuel@gmail.com>
2024-11-18 17:21:32 -0300

    Subindo Repositório

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'favorite-movies-angular';
+  siteName: string = "Favorite Movies";
+
+  img: string ='assets/image.png'; 
 }
